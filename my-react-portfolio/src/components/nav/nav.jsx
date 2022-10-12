80d6c4424d52a07b8aa39d51001bc17edc438ec0
erin-maxson <erin@cohatch.com>
2022-10-11 19:51:44 -0700

    highlighting active nav tabs

--- my-react-portfolio/src/components/nav/nav.jsx
+++ my-react-portfolio/src/components/nav/nav.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import './Nav.css'
-import {IoHome} from 'react-icons/io5'
-import {BsFillPersonFill} from 'react-icons/bs'
-import {AiFillFolderOpen} from 'react-icons/ai'
-import {RiMessage2Fill} from 'react-icons/ri'
+import { IoHome } from 'react-icons/io5'
+import { BsFillPersonFill } from 'react-icons/bs'
+import { AiFillFolderOpen } from 'react-icons/ai'
+import { RiMessage2Fill } from 'react-icons/ri'
+import { useState } from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
+
   return (
     <nav>
-      <a href="#" className='active'><IoHome/></a>
-      <a href="#About"><BsFillPersonFill/></a>
-      <a href="#Projects"><AiFillFolderOpen/></a>
-      <a href="#Contact"><RiMessage2Fill/></a>
+      <a href="#" className={activeNav === '#' ? 'active' : ' '}><IoHome /></a>
+      <a href="#About" onClick={() => setActiveNav('#About')} className={activeNav === '#About' ? 'active' : ' '}><BsFillPersonFill /></a>
+      <a href="#Projects" onClick={() => setActiveNav('#Projects')} className={activeNav === '#Projects' ? 'active' : ' '}><AiFillFolderOpen /></a>
+      <a href="#Contact" onClick={() => setActiveNav('#Contact')} className={activeNav === '#Contact' ? 'active' : ' '}><RiMessage2Fill /></a>
     </nav>
   )
 }
