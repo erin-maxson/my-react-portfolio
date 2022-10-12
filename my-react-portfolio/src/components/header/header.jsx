c2fc5ef0bceede0d39f493dc476c0af3e69c8f0d
erin-maxson <erin@cohatch.com>
2022-10-11 19:05:07 -0700

    working on header component

--- my-react-portfolio/src/components/header/header.jsx
+++ my-react-portfolio/src/components/header/header.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 import './Header.css'
+import Headshot from '../../assets/Erin-Headshot.JPG'
+
 const Header = () => {
   return (
-    <div>header</div>
+    <header>
+      <div className="container.header_container">
+        <h5>Hello I'm</h5>
+        <h1>Erin Maxson</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+
+        <div className="me">
+          <img src="{Headshot}" alt="Erin's Headshot" />
+        </div>
+
+      <a href="#contact" className='scroll_down'>Scroll Down</a>
+      </div>
+
+    </header>
   )
 }
 
