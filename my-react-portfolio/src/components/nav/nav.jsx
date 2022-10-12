e79a64e86d0c18b468146ebb66e8af84d271ecc6
erin-maxson <erin@cohatch.com>
2022-10-11 19:40:03 -0700

    adding icons for nav bar

--- my-react-portfolio/src/components/nav/nav.jsx
+++ my-react-portfolio/src/components/nav/nav.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import './Nav.css'
+import {IoHome} from 'react-icons/io5'
+import {BsFillPersonFill} from 'react-icons/bs'
+import {AiFillFolderOpen} from 'react-icons/ai'
+import {RiMessage2Fill} from 'react-icons/ri'
+
 const Nav = () => {
   return (
-    <div>nav</div>
+    <nav>
+      <a href="#"><IoHome/></a>
+      <a href="#About"><BsFillPersonFill/></a>
+      <a href="#Projects"><AiFillFolderOpen/></a>
+      <a href="#Contact"><RiMessage2Fill/></a>
+    </nav>
   )
 }
 
