8426dbd08bd033d2df97404b92a553b0ccd9dffa
erin-maxson <erin@cohatch.com>
2022-10-11 19:10:25 -0700

    importing react icons

--- my-react-portfolio/src/components/header/HeaderSocials.jsx
+++ my-react-portfolio/src/components/header/HeaderSocials.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+import {VscGithubInverted} from 'react-icons/vsc'
+import {BsFillFilePersonFill} from 'react-icons/bs'
 
 const HeaderSocials = () => {
   return (
     <div className="header_socials">
-        <a href="https://linkedin.com/in/erinmaxson" target="_blank"></a>
-        <a href="https://github.com/erin-maxson" target="_blank"></a>
-        <a href="https://withdogshetravels.com" target="_blank"></a>
+        <a href="https://linkedin.com/in/erinmaxson" target="_blank"><BsLinkedin/></a>
+        <a href="https://github.com/erin-maxson" target="_blank"><VscGithubInverted/></a>
+        <a href="https://withdogshetravels.com" target="_blank"><BsFillFilePersonFill/></a>
     </div>
   )
 }
--- my-react-portfolio/src/components/header/header.jsx
+++ my-react-portfolio/src/components/header/header.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Header.css'
 import Headshot from '../../assets/Erin-Headshot.JPG'
+import HeadshotSocials from './HeaderSocials'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
@@ -9,6 +11,7 @@ const Header = () => {
         <h5>Hello I'm</h5>
         <h1>Erin Maxson</h1>
         <h5 className="text-light">Fullstack Developer</h5>
+        <HeaderSocials />
 
         <div className="me">
           <img src="{Headshot}" alt="Erin's Headshot" />
