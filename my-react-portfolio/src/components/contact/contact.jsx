7f2fc4fc917fb53c87ac5bcb34afbd7cb4a604ae
erin-maxson <erin@cohatch.com>
2022-10-11 22:09:08 -0700

    working on contact form :)

--- my-react-portfolio/src/components/contact/contact.jsx
+++ my-react-portfolio/src/components/contact/contact.jsx
@@ -3,7 +3,26 @@ import './Contact.css'
 
 const Contact = () => {
   return (
-    <section id="Contact">contact</section>
+    <section id="Contact">
+      <h5>Get in Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className='container contact_container'>
+        <div className='contact_options'>
+          <article className="contact_option">
+            <h4>Email</h4>
+            <h5>erin@cohatch.com</h5>
+            <a href="mailto:erin@cohatch.com">Email me here!</a>
+          </article>
+        </div>
+        <form action="">
+          <input type='text' name='name' placeholder='Your Full Name Here' required />
+          <input type='email' name='email' placeholder='yourname@email.com' required />
+          <input type='message' name='message' placeholder='Add your message here' required />
+          <button type='submit' className='btn btn-primary'>Submit</button>
+        </form>
+      </div>
+      </section>
   )
 }
 
