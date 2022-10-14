61ba62e3f6ac48412df71f315a6356f095e3ed34
erin-maxson <erin@cohatch.com>
2022-10-13 17:30:54 -0700

    updating project links

--- my-react-portfolio/src/components/projects/projects.jsx
+++ my-react-portfolio/src/components/projects/projects.jsx
@@ -19,7 +19,7 @@ const Projects = () => {
           <h3>Local Pet</h3>
           <h4>A dog and cat adoption search website.</h4>
           <div className='portfolio_item-cta'>
-            <a href="#" className='btn'>See Github Repo</a>
+            <a href="https://github.com/erin-maxson/cat-and-dog-adoption-search" className='btn'>See Github Repo</a>
           </div>
         </article>
 
@@ -30,7 +30,7 @@ const Projects = () => {
           <h3>HTML + CSS Portfolio</h3>
           <h4>A quick portfolio made in my early days of coding.</h4>
           <div className='portfolio_item-cta'>
-            <a href="#" className='btn'>See Github Repo</a>
+            <a href="https://github.com/erin-maxson/bootcamp-portfolio" className='btn'>See Github Repo</a>
           </div>
         </article>
 
@@ -41,7 +41,7 @@ const Projects = () => {
           <h3>PWA Text Editor</h3>
           <h4>An offline text editor for on-the-go coders.</h4>
           <div className='portfolio_item-cta'>
-            <a href="#" className='btn'>See Github Repo</a>
+            <a href="https://github.com/erin-maxson/pwa-text-editor" className='btn'>See Github Repo</a>
           </div>
         </article>
 
@@ -53,7 +53,7 @@ const Projects = () => {
           <h3>Random Password Generator</h3>
           <h4>Generate random passwords with just a few clicks.</h4>
           <div className='portfolio_item-cta'>
-            <a href="#" className='btn'>See Github Repo</a>
+            <a href="https://github.com/erin-maxson/password-generator" className='btn'>See Github Repo</a>
           </div>
           </article>
 
@@ -64,7 +64,7 @@ const Projects = () => {
           <h3>Coder's Almanac</h3>
           <h4>A digital seed library made for the tech-savvy.</h4>
           <div className='portfolio_item-cta'>
-            <a href="#" className='btn'>See Github Repo</a>
+            <a href="https://github.com/erin-maxson/coders-almanac" className='btn'>See Github Repo</a>
           </div>
           </article>
 
@@ -75,7 +75,7 @@ const Projects = () => {
           <h3>Tech Blog</h3>
           <h4>A blog to find things related to technology.</h4>
           <div className='portfolio_item-cta'>
-            <a href="#" className='btn'>See Github Repo</a>
+            <a href="https://github.com/erin-maxson/tech-blog" className='btn'>See Github Repo</a>
           </div>
           </article>
       </div>
