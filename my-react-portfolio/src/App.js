6c7637f6d0998bb1840b62de733ff21330d20a9c
erin-maxson <erin@cohatch.com>
2022-10-13 17:34:13 -0700

    adding readme and updating formatting

--- my-react-portfolio/src/App.js
+++ my-react-portfolio/src/App.js
@@ -3,7 +3,6 @@ import Nav from './components/Nav/Nav'
 import About from './components/About/About'
 import Projects from './components/Projects/Projects'
 import Contact from './components/Contact/Contact'
-import Footer from './components/Footer/Footer'
 import './App.css';
 
 function App() {
@@ -14,7 +13,6 @@ function App() {
      <About />
      <Projects />
      <Contact />
-     <Footer />
     </div>
   );
 }
